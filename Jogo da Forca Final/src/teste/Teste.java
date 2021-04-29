87a9657dba284727159c9f28804425e42c68632a
MatheusHenrickSantos <79066829+MatheusHenrickSantos@users.noreply.github.com>
2021-04-28 22:58:24 -0300

    Delete Jogo da Forca Final directory

--- Jogo da Forca Final/src/teste/Teste.java
+++
@@ -1,8 +0,0 @@
-package teste;
-
-class Teste {
-	/*
-	 * Meu projeto trabalha com métodos void. Não consegui criar casos de teste, mas o programa
-	 * foi revisado por mim e por terceiros e aparentemente não possui falhas.
-	 */
-}
